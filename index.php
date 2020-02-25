94aac6b5cf9e3d22c13a4202704ab66eda347c84
Gianufio83 <fiore.gianuario@gmail.com>
2020-02-25 16:14:04 +0100

    preparazione struttura html con Sass

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+  include __DIR__.'/database.php';
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="dist/app.css">
+   <title>Dischi</title>
+ </head>
+ <body>
+   <header>
+     <div class="header-top">
+       <img src="dist/img/icon.png" alt="">
+     </div>
+   </header>
+   <main>
+       <div class="wrapper">
+         <div class="first">
+            <div class="box">
+              <img src="" alt="">
+              <h3></h3>
+              <p></p>
+              <small></small>
+            </div>
+            <div class="box">
+              <img src="" alt="">
+              <h3></h3>
+              <p></p>
+              <small></small>
+            </div>
+            <div class="box">
+              <img src="" alt="">
+              <h3></h3>
+              <p></p>
+              <small></small>
+            </div>
+         </div>
+         <div class="second">
+           <div class="box">
+             <img src="" alt="">
+             <h3></h3>
+             <p></p>
+             <small></small>
+           </div>
+           <div class="box">
+             <img src="" alt="">
+             <h3></h3>
+             <p></p>
+             <small></small>
+           </div>
+           <div class="box">
+             <img src="" alt="">
+             <h3></h3>
+             <p></p>
+             <small></small>
+           </div>
+         </div>
+       </div>
+   </main>
+   <footer>
+
+   </footer>
+ </body>
+ </html>
