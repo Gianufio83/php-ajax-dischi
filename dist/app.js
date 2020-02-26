73ae126adb8575962f7933d782e8df598b4c31cc
Gianufio83 <fiore.gianuario@gmail.com>
2020-02-26 14:27:33 +0100

    funzionamento j query corretto -seconda parte esercizio completa

--- dist/app.js
+++ dist/app.js
@@ -15823,38 +15823,40 @@ $(document).ready(function () {
     url: 'http://localhost/php-ajax-dischi/server.php',
     method: 'GET',
     success: function success(data) {
-      if (data.lenght > 0) {
-        printDischi(dischi);
-      } else {
-        discoNotFound();
-      }
+      console.log(data); // printDischi(data);
 
-      ;
+      var source = $('#dischi-template').html();
+      var template = Handlebars.compile(source);
+
+      for (var i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        var context = data[i];
+        var html = template(context);
+        $('.first').append(html);
+      }
     },
     error: function error() {
       alert('Errore');
+      discoNotFound();
     }
   });
-});
-
-function printDischi(dischi) {
-  var source = $('#dischi-template').html();
-  var template = Handlebars.compile(source);
-
-  for (var i = 0; i < dischi.length; i++) {
-    var disco = disco[i];
-    var context = {
-      poster: disco.poster,
-      title: disco.title,
-      author: disco.author,
-      year: disco.year
-    };
-    var html = template(context);
-    $('.first').append(html);
-  }
-}
-
-;
+}); // function printDischi(dischi) {
+//
+//   var source = $('#dischi-template').html();
+//   var template = Handlebars.compile(source);
+//
+//   for (var i = 0; i < dischi.length; i++) {
+//      var disco = disco[i];
+//      var context = {
+//        poster : disco.poster,
+//        title : disco.title,
+//        author : disco.author,
+//        year: disco.year,
+//      }
+//      var html = template(context);
+//      $('.first').append(html);
+//   }
+// };
 
 function discoNotFound() {
   var source = $('#noresults-template').html();
--- src/app.js
+++ src/app.js
@@ -8,43 +8,47 @@ $(document).ready(function(){
     url:'http://localhost/php-ajax-dischi/server.php',
     method: 'GET',
     success: function (data) {
+      console.log(data);
+      // printDischi(data);
 
-    if (data.lenght > 0) {
-        printDischi(dischi);
-          }
-          else {
-          discoNotFound();
-          };
+        var source = $('#dischi-template').html();
+        var template = Handlebars.compile(source);
+        for (var i = 0; i < data.length; i++) {
+         console.log(data[i]);
+         var context = data[i];
+         var html = template(context);
+         $('.first').append(html);
+
+    }
     },
     error: function () {
       alert('Errore');
+      discoNotFound();
     }
   }
   );
 });
 
-function printDischi(dischi) {
-
-  var source = $('#dischi-template').html();
-  var template = Handlebars.compile(source);
-
-  for (var i = 0; i < dischi.length; i++) {
-     var disco = disco[i];
-     var context = {
-       poster : disco.poster,
-       title : disco.title,
-       author : disco.author,
-       year: disco.year,
-     }
-     var html = template(context);
-     $('.first').append(html);
-  }
-};
+// function printDischi(dischi) {
+//
+//   var source = $('#dischi-template').html();
+//   var template = Handlebars.compile(source);
+//
+//   for (var i = 0; i < dischi.length; i++) {
+//      var disco = disco[i];
+//      var context = {
+//        poster : disco.poster,
+//        title : disco.title,
+//        author : disco.author,
+//        year: disco.year,
+//      }
+//      var html = template(context);
+//      $('.first').append(html);
+//   }
+// };
 function discoNotFound() {
     var source = $('#noresults-template').html();
     var template = Handlebars.compile(source);
     var html = template();
     $('.first').append(html);
-
-
   };
